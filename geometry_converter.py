50b93d6d3e17a39f1387238a60e97b32b27d4189
Tom Schäfer <tschaefer.acc@gmail.com>
2026-07-05 21:33:41 +0200

    geometry_converter with working hash id's, overall correct export (final-candidate)

--- geometry_converter.py
+++ geometry_converter.py
@@ -53,6 +53,10 @@ def convert_nodegroup_to_xml(nodegroup, root):
 
         # mostly properties regarding graphical representation in blender
         filter_unnecessary = {
+        'type',
+        'name',
+        'label',
+        
         'width',
         'height',
         'use_custom_color',
@@ -62,6 +66,8 @@ def convert_nodegroup_to_xml(nodegroup, root):
         'show_preview',
         'hide',
         'show_texture',
+        'internal_links',
+        'warning_propagation',
 
         'bl_idname',
         'bl_label',
@@ -92,7 +98,7 @@ def convert_nodegroup_to_xml(nodegroup, root):
                 continue
             prop = getattr(node, prop_name)
 
-            # collection properties (inputs, outputs, internal_links)
+            # collection properties (inputs, outputs)
             if isinstance(prop, bpy.types.bpy_prop_collection):
                 convert_bpy_collection_to_xml(prop, prop_name, node_element)
 
@@ -133,8 +139,8 @@ def convert_nodegroup_to_xml(nodegroup, root):
     # Socket is the connection point (variable) from the graph
     
     for link in nodegroup.links:
-        from_id = port_id_hash(link.from_node.name, link.from_socket.name)
-        to_id = port_id_hash(link.to_node.name, link.to_socket.name)
+        from_id = port_id_hash(link.from_node.name, link.from_socket.as_pointer())
+        to_id = port_id_hash(link.to_node.name, link.to_socket.as_pointer())
         connection_element = ET.SubElement(
             nodegroup_element,
             "Connection"
@@ -169,24 +175,24 @@ def convert_mathutils_euler_to_xml(prop, prop_name, parent_element):
 
 def convert_bpy_collection_to_xml(prop, prop_name, parent_element):
     try:
-        for item in prop.keys():
-            if prop.get(item) is None:
+        for item in prop:
+            if item is None:
                 continue
-            item = prop.get(item)
 
             if item.is_linked:
-                item_element = ET.SubElement(parent_element, "Port", name=item.name, direction="out" if item.is_output else "in", id=port_id_hash(parent_element.get("name"), item.name))
+                item_element = ET.SubElement(parent_element, "Port", name=item.name, direction="out" if item.is_output else "in", id=port_id_hash(parent_element.get("name"), item.as_pointer()))
             else:
                 if item.is_output:
                     continue  # Skip unlinked output items
+                
                 if hasattr(item, 'default_value'):
                     if isinstance(item.default_value, bpy.types.bpy_prop_array):
-                        item_element = ET.SubElement(parent_element, "Port", name="vectorIn", direction="in", id=port_id_hash(parent_element.get("name"), "vectorIn"))
+                        item_element = ET.SubElement(parent_element, "Port", name="vectorIn", direction="in", id=port_id_hash(parent_element.get("name"), item.as_pointer()))
                         extracted_vec_element = ET.SubElement(parent_element.getparent(), "Node", name=item.name, type=str(getattr(item, 'type', None)))
                         vec_coordinate_names = ['x', 'y', 'z']
                         for i in range(3):
                             ET.SubElement(extracted_vec_element, "Constant", name=vec_coordinate_names[i], value=str(item.default_value[i]))
-                        extracted_vec_element_outsocket = ET.SubElement(extracted_vec_element, "Port", name="vectorOut", direction="out", id=port_id_hash(parent_element.get("name"), "vectorOut"))
+                        extracted_vec_element_outsocket = ET.SubElement(extracted_vec_element, "Port", name="vectorOut", direction="out", id=port_id_hash(parent_element.get("name"), f"{item.as_pointer()}vectorOut"))
 
                         ET.SubElement(parent_element.getparent(), "Connection", from_=extracted_vec_element_outsocket.get("id"), to=item_element.get("id"))
                     else:
@@ -201,5 +207,5 @@ def convert_bpy_collection_to_xml(prop, prop_name, parent_element):
 # Helper Methods #
 ##################
 
-def port_id_hash(parent_name, item_name):
-    return hashlib.sha1(f'{parent_name}{item_name}'.encode()).hexdigest()
+def port_id_hash(parent_name, item_pointer):
+    return hashlib.sha1(f'{parent_name}{item_pointer}'.encode()).hexdigest()
